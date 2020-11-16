e43b9e3380f3cc5bba7d8ebe959dd16ee0c9767b
elmehdielbachiri <44084329+elmehdielbachiri@users.noreply.github.com>
2020-11-16 15:48:34 +0100

    Delete Ranking.R

--- Ranking.R
+++
@@ -1,9 +0,0 @@
-Ranking<-function(matrixrank,method){
-  if(method=="bourda"){
-    Bestindex=which.max(rowSums(matrixrank))
-  } else if (method=="Parisa"){
-    Bestindex=pari(matrixrank)
-  } else if (method=="Rastin"){
-    Bestindex=Rast(matrixrank)
-  }
-  return(Bestindex)}
